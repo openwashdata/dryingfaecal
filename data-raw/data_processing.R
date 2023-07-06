4a8f2e049797b8de7a54e1ad85181c8dfa309852
Mian <mzhong@ethz.ch>
2023-07-06 17:45:15 +0200

    add feedstock

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -4,6 +4,7 @@ library(stringr)
 library(dplyr)
 library(tidyr)
 library(tibble)
+## Some reference from this amazing blog post: https://www.brodrigues.co/blog/2018-06-10-scraping_pdfs/
 
 # Split the original pdf into different chapters according to toc
 master <- "./data-raw/gatesopenres-196902.pdf"
@@ -81,6 +82,58 @@ extract_data_source_links <- function(txt, start, end){
   return(links)
 }
 
+# extract feedstock
+extract_feedstock <- function(txt, start, end){
+  txt <- str_sub(txt, start, end) |>
+    str_replace_all("\n{2,}", "\n") |>
+    str_trim()
+
+  num_newline <- str_count(txt, "\n")
+  if (num_newline==8) {
+    feedstock <- txt |>
+      str_split_1("\n") |>
+      str_trim() |>
+      str_replace("\\s{2,}", "@")
+
+    feedstock <- separate(tibble(feedstock), col="feedstock", into = c("key", "values"), sep = "@")
+  } else {
+    values <- txt |>
+      str_replace("\n* *Type of faecal material\n*", replacement = "") |>
+      str_replace("Location of collection\n*", replacement = "") |>
+      str_replace("Age before collection\n*", replacement = "") |>
+      str_replace("Moisture content\n*", replacement = "") |>
+      str_replace("Total solids content\n*", replacement = "") |>
+      str_replace("Volatile solids content\n*", replacement = "") |>
+      str_replace("Ash content\n*", replacement = "") |>
+      str_replace("\n* *Presence of trash\\?\n*", replacement = "") |>
+      str_replace("Pre-treatment\n*", replacement = "")
+    if (str_count(values, "\n") > 8){
+      newline_idx <- str_locate_all(values, "\n")[[1]][,1]
+      squish_start <- newline_idx[1]+1
+      squish_end <- tail(newline_idx, n=1)-1
+      middle <- str_squish(str_sub(values, squish_start,squish_end))
+      values <- paste(str_sub(values, 1, squish_start-1),
+                      middle,
+                      str_sub(values, squish_end+1))
+    }
+    values <- values |>
+      str_replace_all("\n", "@") |>
+      str_squish() |>
+      str_split_1("@ ")
+    feedstock <- tibble(key=c("Type of faecal material",
+                              "Location of collection",
+                              "Age before collection",
+                              "Moisture content",
+                              "Total solids content",
+                              "Volatile solids content",
+                              "Ash content",
+                              "Presence of trash?",
+                              "Pre-treatment"),
+                      values = values)
+  }
+  return(feedstock)
+}
+
 # convert a chapter pdf to tibble tables
 pdf_to_tibble <- function(pdf){
   # Convert pdf in text
@@ -98,25 +151,42 @@ pdf_to_tibble <- function(pdf){
   n_tables <- nrow(geninfo_idx)
   tbl_list <- list()
   for (j in 1:n_tables) {
-    tryCatch(
+    geninfo <- extract_general_info(txt, geninfo_idx[j, 2]+1, feedstock_idx[j, 1]-1) |>
+      add_row(key = "publication", values = extract_data_source_links(txt, pub_idx[j, 2]+1, datasource_idx[j, 1]-1)) |>
+      add_row(key = "link", values = extract_data_source_links(txt, datasource_idx[j, 2]+1, addnotes_idx[j, 1]-1))
+    tbl_list[[j]] <- tryCatch(
       {
-        tbl_list[[j]] <- extract_general_info(txt, geninfo_idx[j, 2]+1, feedstock_idx[j, 1]-1) |>
-          add_row(key = "publication", values = extract_data_source_links(txt, pub_idx[j, 2]+1, datasource_idx[j, 1]-1)) |>
-          add_row(key = "link", values = extract_data_source_links(txt, datasource_idx[j, 2]+1, addnotes_idx[j, 1]-1)) |>
-          pivot_wider(names_from = key, values_from = values)},
+        feedstock <- extract_feedstock(txt, feedstock_idx[j, 2]+1, exp_idx[j, 1]-1)
+        tbl_list[[j]] <- bind_rows(geninfo, feedstock) |>
+          pivot_wider(names_from = key, values_from = values)
+        },
       error = function(e) {
         print(paste(chaptername, j))
-        print(e)
-        print(substr(txt, geninfo_idx[j,2]+1, feedstock_idx[j, 1]-1))
-
+        print(str_trim(str_sub(txt, feedstock_idx[j, 2]+1, exp_idx[j, 1]-1)))
+        feedstock <- tibble(key=c("Type of faecal material",
+                                  "Location of collection",
+                                  "Age before collection",
+                                  "Moisture content",
+                                  "Total solids content",
+                                  "Volatile solids content",
+                                  "Ash content",
+                                  "Presence of trash?",
+                                  "Pre-treatment"),
+                            values = NA)
+        tb <- bind_rows(geninfo, feedstock) |>
+          pivot_wider(names_from = key, values_from = values)
+        return(tb)
         }
     )
+
   }
   chapter_tbl <- bind_rows(tbl_list)
-  chapter_tbl <- chapter_tbl |> add_column(chapter=chaptername, .before = "Type of data")
+  chapter_tbl <- chapter_tbl |>
+    add_column(chapter=chaptername, .before = "Type of data")
   return(chapter_tbl)
 }
 
+# Convert pdf metadata tables into tibble
 pdffiles <- c(dewatering, disinfection, gas, kinestics, mech, morphilogical, physiochemical, thermodynamics)
 chapter_tbl_list <- list()
 
@@ -125,7 +195,9 @@ for (i in 1:length(pdffiles)) {
   chapter_tbl_list[[i]] <- chapter_tbl
 }
 
-addendum <- bind_rows(chapter_tbl_list)
+addendum <- tibble(bind_rows(chapter_tbl_list))
+# Add buggy tables manually
+
 addendum <- addendum |>
   add_column(table_id = 1:nrow(addendum), .before = "chapter") |>
   rename(type=`Type of data`,
